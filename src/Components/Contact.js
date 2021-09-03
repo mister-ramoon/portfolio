8d9d926547d7badfd95d18869045f0f6591fa276
ramonruizrc <ramonruizrc@gmail.com>
2021-09-03 12:16:07 -0500

    Testimonials, Form and Footer

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { send } from "emailjs-com";
 import "../Styles/Components/Contact.scss";
 
 function Contact() {
-  return <div></div>;
+  const [toSend, setToSend] = useState({
+    from_name: "",
+    to_name: "",
+    message: "",
+    reply_to: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      "service_tyoo6qp",
+      "template_hzc46su",
+      toSend,
+      "user_II3R7MQVCcVq5PhnQ34ey"
+    )
+      .then((response) => {
+        console.log("Correo enviado!", response.status, response.text);
+      })
+      .catch((err) => {
+        console.error("Error! ...", err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="contact__container">
+      <h2 className="title">Hablemos</h2>
+      <form onSubmit={onSubmit} className="contact__form">
+        <label className="contact__label">Nombre</label>
+        <input
+          type="text"
+          name="from_name"
+          className="contact__input"
+          value={toSend.from_name}
+          onChange={handleChange}
+        />
+        <label className="contact__label">Correo</label>
+        <input
+          type="text"
+          name="reply_to"
+          className="contact__input"
+          value={toSend.reply_to}
+          onChange={handleChange}
+        />
+        <label className="contact__label">Mensaje</label>
+        <textarea
+          type="text"
+          name="message"
+          rows="5"
+          className="contact__input"
+          value={toSend.message}
+          onChange={handleChange}
+        />
+        <input className="contact__btn" type="submit" value="Enviar" />
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,58 @@
 import React from "react";
 import "../Styles/Components/Footer.scss";
+import githubIcon from "../Icons/Github.svg";
+import twitterIcon from "../Icons/Twitter.svg";
+import LinkedinIcon from "../Icons/Linkedin.svg";
+import PlatziIcon from "../Icons/Platzi.svg";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="footer__container">
+      <footer className="footer">
+        <div className="footer__copy">
+          <h5 className="footer__copy--text">Creado por @ramonruizdev</h5>
+        </div>
+        <ul className="hero__list">
+          <li className="hero__list--item">
+            <a href="/" target="_blank" className="hero__list--link">
+              <img
+                className="hero__list--img"
+                alt="Social Icon"
+                src={LinkedinIcon}
+              />
+            </a>
+          </li>
+          <li className="hero__list--item">
+            <a href="/" target="_blank" className="hero__list--link">
+              <img
+                className="hero__list--img"
+                alt="Social Icon"
+                src={githubIcon}
+              />
+            </a>
+          </li>
+          <li className="hero__list--item">
+            <a href="/" target="_blank" className="hero__list--link">
+              <img
+                className="hero__list--img"
+                alt="Social Icon"
+                src={PlatziIcon}
+              />
+            </a>
+          </li>
+          <li className="hero__list--item">
+            <a href="/" target="_blank" className="hero__list--link">
+              <img
+                className="hero__list--img"
+                alt="Social Icon"
+                src={twitterIcon}
+              />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
